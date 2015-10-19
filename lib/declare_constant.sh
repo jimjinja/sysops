7cd2f95e528857be2ee7a8b340de5bc18627d0f9
Jingun Jung <i@koreabigname.com>
2015-10-20 01:00:50 +0900

    Finish script-comm-constant

---
+++ lib/declare_constant.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+################################################################################
+# Title     : Declare constants
+# Developer : Jingun Jung
+# Shebang   : bash
+################################################################################
+export RET_CD_SUCCESS_ALL=0
+export RET_CD_FATAL_ERROR=255
